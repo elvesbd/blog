56795f9a63ee8db8aaa0c9e4ee2e213ec5c8be40
elvesbd <elvesbd@yahoo.com>
2023-06-16 14:49:54 -0300

    feat: created tag management

--- Screens/TagScreens/DeleteTagScreen.cs
+++ Screens/TagScreens/DeleteTagScreen.cs
@@ -1,8 +1,37 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.TagScreens
 {
     public static class DeleteTagScreen
     {
         public static void Load()
-        { }
+        {
+            Console.Clear();
+            Console.WriteLine("Deletando uma tag");
+            Console.WriteLine("--------------------");
+
+            Console.Write("Digite o ID da Tag que deseja excluir: ");
+            var id = Console.ReadLine();
+
+            Delete(int.Parse(id));
+            Console.ReadKey();
+            MenuTagScreen.Load();
+        }
+
+        public static void Delete(int id)
+        {
+            try
+            {
+                var repository = new Repository<Tag>(Database.Connection);
+                repository.Delete(id);
+                Console.WriteLine("Tag excluída com sucesso!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Não foi possível salvar a tag");
+                Console.WriteLine(ex.Message);
+            }
+        }
     }
 }
--- Screens/TagScreens/ListTagsScreen.cs
+++ Screens/TagScreens/ListTagsScreen.cs
@@ -1,7 +1,28 @@
+using Blog.Models;
+using Blog.Repositories;
+
 namespace Blog.Screens.TagScreens
 {
     public static class ListTagsScreen
     {
-        public static void Load() { }
+        public static void Load()
+        {
+            Console.Clear();
+            Console.WriteLine("Lista de tags");
+            Console.WriteLine("--------------------");
+            List();
+            Console.ReadKey();
+            MenuTagScreen.Load();
+        }
+
+        public static void List()
+        {
+            var repository = new Repository<Tag>(Database.Connection);
+            var tags = repository.Get();
+            foreach (var tag in tags)
+            {
+                Console.WriteLine($"{tag.Id} - {tag.Name} ({tag.Slug})");
+            }
+        }
     }
 }
